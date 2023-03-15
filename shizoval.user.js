cebce1e63b1de3f4a795c234d1cf63e1b222da37
sheezzmee <106715792+sheezzmee@users.noreply.github.com>
2023-03-15 19:12:07 +0300

    Create shizoval.user.js

---
+++ shizoval.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         shizoval
+// @version      1.0.0
+// @description  Free game cheat for Tanki Online.
+// @author       sheezzmee
+// @match        https://*.tankionline.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tankionline.com
+
+// @require      https://raw.githubusercontent.com/flyover/imgui-js/master/dist/imgui.umd.js
+// @require      https://raw.githubusercontent.com/flyover/imgui-js/master/dist/imgui_impl.umd.js
+
+// @run-at       document-start
+// @grant        GM_xmlhttpRequest
+// @grant        unsafeWindow
+
+// ==/UserScript==
+
+GM_xmlhttpRequest({
+    method: 'GET',
+    url: 'https://raw.githubusercontent.com/sheezzmee/shizoval/main/shizoval.min.js',
+    nocache: true,
+    onload: data => eval(data.responseText)
+})
